f5006d1826fd1f7e62aeca04895dc83155291c39
Watwax <watwax86@gmail.com>
2025-11-29 13:38:42 +0100

    Add a new class to manage player turn and modify indication text into the game

---
+++ gamePlayer.js
@@ -0,0 +1,58 @@
+export class GamePlayer {
+    constructor(uiElementId) {
+        this.players = ["Joueur 1", "Joueur 2"];
+        this.current = 0; // Index of player in the array
+        this.state = "select"; 
+        /*
+            Only 2 states possible : 
+            - "select" : select a pawn for the opponent
+            - "place"  : place the pawn given by the opponent
+        */
+
+        this.ui = document.getElementById(uiElementId);
+        this.updateText();
+    }
+
+    // Return the actual player (string of his name)
+    get currentPlayer() {
+        return this.players[this.current];
+    }
+
+    // Return the opponent player
+    get opponentPlayer() {
+        return this.players[(this.current + 1) % 2];
+    }
+
+    // Update the text with turn's state
+    updateText() {
+        if (this.state === "select") {
+            this.ui.innerText = 
+                `${this.currentPlayer} : sélectionnez une pièce pour ${this.opponentPlayer}`;
+        } 
+        else if (this.state === "place") {
+            this.ui.innerText = 
+                `${this.currentPlayer} : placez la pièce que ${this.opponentPlayer} vous a donnée`;
+        }
+    }
+
+    // When a pawn is selected
+    onSelectPiece() {
+        if (this.state !== "select") return false;
+
+        // The turn is now in state "place the pawn"
+        this.state = "place";
+        this.updateText();
+        return true;
+    }
+
+    // When a pawn is placed into the board
+    onPlacePiece() {
+        if (this.state !== "place") return false;
+
+        // End of the turn, the player change
+        this.current = (this.current + 1) % 2;
+        this.state = "select";
+        this.updateText();
+        return true;
+    }
+}
